7a8d7a89182f40fb294fc114fd17ae5e241cda8e
Davide Ceneda <dceneda@yahoo.it>
2014-09-10 18:27:39 +0200

    added labels to node

--- morphing.js
+++ morphing.js
@@ -5,7 +5,7 @@
 d3.json("data/skips.json", function(err, data) {
 
 
-
+var i = 0;
 
     var svg = d3.select("body").append("svg")
         .attr("width", 1200)
@@ -19,12 +19,27 @@ d3.json("data/skips.json", function(err, data) {
         .style("stroke", "Black")
         .style('stroke-width', '3px');
 
+
+
+
+
     var nodes = svg.selectAll(".node")
-        .data(data.nodes).enter().append("svg:circle")
-        .classed("node", true)
-        .attr("r", 15)
-        .attr("cx", function(d) { return d[0].x; })
-        .attr("cy", function(d) { return d[0].y; });
+        .data(data.nodes)
+        .enter().append("g").attr("class", "node")
+        .attr("transform", function(d) { return "translate("+d[0].x+","+d[0].y+")"; })
+
+
+    nodes.append("circle")
+        .attr("class", "node")
+        .attr("r", 20);
+
+    nodes.append("text")
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d[0].name;
+        }).style("fill","White");
+
+
 
     var anim = function (index, duration) {
         edges.transition()
@@ -36,8 +51,7 @@ d3.json("data/skips.json", function(err, data) {
 
         nodes.transition()
             .duration(duration)
-            .attr("cx", function(d) { return d[index].x; })
-            .attr("cy", function(d) { return d[index].y; })
+            .attr("transform", function(d) { return "translate("+d[index].x+","+d[index].y+")"; })
             .each("end",function() {d3.select(this).style("fill","Black")});
 
         if(index < data.ops) {
@@ -50,8 +64,6 @@ d3.json("data/skips.json", function(err, data) {
 
 
     var animate = function(index, duration) {
-
-        //.attr("d", function(datum) { return datum[index].d; })
 
         console.log(index)
 
